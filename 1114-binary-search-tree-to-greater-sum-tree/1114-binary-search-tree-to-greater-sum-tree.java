fe561d09565eac369e5b29bcc2d806ecee052e91
V K NITHYANAND <127433644+nithyanandvk@users.noreply.github.com>
2025-06-03 17:10:45 +0530

    Time: 0 ms (100%), Space: 41.7 MB (15.03%) - LeetHub

--- 1114-binary-search-tree-to-greater-sum-tree/1114-binary-search-tree-to-greater-sum-tree.java
+++ 1114-binary-search-tree-to-greater-sum-tree/1114-binary-search-tree-to-greater-sum-tree.java
@@ -1,27 +1,11 @@
 class Solution {
-    ArrayList<Integer> l = new ArrayList<>();
-    HashMap<Integer, Integer> map = new HashMap<>();
-
-    void inorder(TreeNode root) {
-        if (root == null) return;
-        inorder(root.left);
-        l.add(root.val);
-        inorder(root.right);
-    }
-
+    int sum = 0;
     public TreeNode bstToGst(TreeNode root) {
-        if (l.size() == 0) {
-            inorder(root);
-            int sum = 0;
-            for (int i = l.size() - 1; i >= 0; i--) {
-                sum += l.get(i);
-                map.put(l.get(i), sum);
-            }
-        }
         if (root == null) return null;
-        root.val = map.get(root.val);
-        root.left = bstToGst(root.left);
-        root.right = bstToGst(root.right);
+        bstToGst(root.right);
+        sum += root.val;
+        root.val = sum;
+        bstToGst(root.left);
         return root;
     }
 }
