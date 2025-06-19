512061d15cd40fcae2ee54e303d53e3cf4c2fc3e
V K NITHYANAND <127433644+nithyanandvk@users.noreply.github.com>
2025-06-19 12:17:21 +0530

    Time: 8 ms (56.94%), Space: 45.5 MB (63.13%) - LeetHub

--- 2502-sort-the-people/2502-sort-the-people.java
+++ 2502-sort-the-people/2502-sort-the-people.java
@@ -1,18 +1,37 @@
 class Solution {
-    public String[] sortPeople(String[] names, int[] heights) {
-       int n=heights.length;
-       for(int i=0;i<n;i++){
-        for(int j=0;j<n-i-1;j++){
-            if(heights[j]<heights[j+1]){
-                int temp=heights[j];
-                heights[j]=heights[j+1];
-                heights[j+1]=temp;
-                String t=names[j];
-                names[j]=names[j+1];
-                names[j+1]=t;
-            }
+    class Person {
+        String name;
+        int ht;
+
+        Person(String name, int ht) {
+            this.name = name;
+            this.ht = ht;
         }
-       } 
-       return names;
+    }
+
+    public String[] sortPeople(String[] names, int[] heights) {
+        int n = heights.length;
+        //    for(int i=0;i<n-1;i++){
+        //     for(int j=0;j<n-i-1;j++){
+        //         if(heights[j]<heights[j+1]){
+        //             int temp=heights[j];
+        //             heights[j]=heights[j+1];
+        //             heights[j+1]=temp;
+        //             String t=names[j];
+        //             names[j]=names[j+1];
+        //             names[j+1]=t;
+        //         }
+        //     }
+        //    } 
+        //    return names;
+        ArrayList<Person> l = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            l.add(new Person(names[i],heights[i]));
+        }
+        Collections.sort(l,(a,b)-> b.ht-a.ht);
+        for(int i=0;i<n;i++){
+            names[i]=l.get(i).name;
+        }
+        return names;
     }
 }
