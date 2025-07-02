ac72082982a4b7cb2c78bf4e3d0b0f1ac42c4e74
V K NITHYANAND <127433644+nithyanandvk@users.noreply.github.com>
2025-07-02 11:56:13 +0530

    Time: 5 ms (89.35%), Space: 44.8 MB (42.23%) - LeetHub

--- 2204-find-subsequence-of-length-k-with-the-largest-sum/2204-find-subsequence-of-length-k-with-the-largest-sum.java
+++ 2204-find-subsequence-of-length-k-with-the-largest-sum/2204-find-subsequence-of-length-k-with-the-largest-sum.java
@@ -1,30 +1,32 @@
-import java.util.*;
-
 class Solution {
-    class Point {
+    static class point implements Comparable<point>{
         int data;
         int i;
-        Point(int data, int i) {
-            this.data = data;
+        public point(int d,int i){
+            this.data = d;
             this.i = i;
         }
+        @Override
+        public int compareTo(point a){
+            return a.data - this.data;
+        }
     }
-
     public int[] maxSubsequence(int[] nums, int k) {
-        ArrayList<Point> list = new ArrayList<>();
-        for (int i = 0; i < nums.length; i++) {
-            list.add(new Point(nums[i], i));
+        int arr[] = new int[k];
+        ArrayList<point> li = new ArrayList<>();
+        PriorityQueue<point> q = new PriorityQueue<>();
+        int j=0;
+        for(int i=0;i<nums.length;i++){
+            q.add(new point(nums[i],i));
         }
-
-        Collections.sort(list, (a, b) -> b.data - a.data);
-        List<Point> topK = list.subList(0, k);
-        Collections.sort(topK, (a, b) -> a.i - b.i);
-
-        int[] ans = new int[k];
-        for (int i = 0; i < k; i++) {
-            ans[i] = topK.get(i).data;
+        while(j<k){
+            arr[j] = q.remove().i;
+            j++;
         }
-
-        return ans;
+        Arrays.sort(arr);
+        for(int i=0;i<k;i++){
+            arr[i] = nums[arr[i]];
+        }
+        return arr;
     }
 }
