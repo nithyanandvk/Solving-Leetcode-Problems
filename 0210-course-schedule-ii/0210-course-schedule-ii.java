63b569d6fd23453de28df728259130be891600d6
V K NITHYANAND <127433644+nithyanandvk@users.noreply.github.com>
2025-07-09 00:08:36 +0530

    Time: 4 ms (92.15%), Space: 46.2 MB (29.88%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,30 +1,72 @@
+// class Solution {
+//     public int[] findOrder(int n, int[][] pre) {
+//         int in[] = new int[n];
+//         for (int i = 0; i < pre.length; i++) {
+//             in[pre[i][0]]++;
+//         }
+
+//         int ans[] = new int[n];
+//         int x = 0;
+//         Queue<Integer> q = new LinkedList<>();
+//         for (int i = 0; i < in.length; i++) {
+//             if (in[i] == 0) q.add(i);
+//         }
+
+//         while (!q.isEmpty()) {
+//             int curr = q.poll();
+//             ans[x++] = curr;
+//             for (int i = 0; i < pre.length; i++) {
+//                 if (pre[i][1] == curr) {
+//                     in[pre[i][0]]--;
+//                     if (in[pre[i][0]] == 0) {
+//                         q.add(pre[i][0]);
+//                     }
+//                 }
+//             }
+//         }
+
+//         return x == n ? ans : new int[0];
+//     }
+// }
+
 class Solution {
     public int[] findOrder(int n, int[][] pre) {
-        int in[] = new int[n];
-        for (int i = 0; i < pre.length; i++) {
-            in[pre[i][0]]++;
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            graph.add(new ArrayList<>());
         }
 
-        int ans[] = new int[n];
-        int x = 0;
-        Queue<Integer> q = new LinkedList<>();
-        for (int i = 0; i < in.length; i++) {
-            if (in[i] == 0) q.add(i);
+        int[] in = new int[n];
+        for (int[] p : pre) {
+            graph.get(p[1]).add(p[0]);
+            in[p[0]]++;
         }
+
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            if (in[i] == 0) {
+                q.add(i);
+            }
+        }
+
+        int[] ans = new int[n];
+        int idx = 0;
 
         while (!q.isEmpty()) {
             int curr = q.poll();
-            ans[x++] = curr;
-            for (int i = 0; i < pre.length; i++) {
-                if (pre[i][1] == curr) {
-                    in[pre[i][0]]--;
-                    if (in[pre[i][0]] == 0) {
-                        q.add(pre[i][0]);
-                    }
+            ans[idx++] = curr;
+            for (int next : graph.get(curr)) {
+                in[next]--;
+                if (in[next] == 0) {
+                    q.add(next);
                 }
             }
         }
 
-        return x == n ? ans : new int[0];
+        if (idx == n) {
+            return ans;
+        } else {
+            return new int[0];
+        }
     }
 }
