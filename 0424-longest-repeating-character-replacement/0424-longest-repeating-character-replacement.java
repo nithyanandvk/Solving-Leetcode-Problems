4d90fb374795946237a7046443f856f4a094b5fd
V K NITHYANAND <127433644+nithyanandvk@users.noreply.github.com>
2025-07-19 10:38:02 +0530

    Time: 10 ms (67.99%), Space: 45.1 MB (40.07%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -10,8 +10,9 @@ class Solution {
             char c = s.charAt(r);
             arr[c - 'A']++;
             maxCount = Math.max(maxCount, arr[c - 'A']);
-            if ((r - l + 1) - maxCount > k) {
+            while ((r - l + 1) - maxCount > k) {
                 arr[s.charAt(l) - 'A']--;
+                maxCount=maxi(arr);
                 l++;
             }
             ans = Math.max(ans, r - l + 1);
@@ -19,5 +20,12 @@ class Solution {
         }
 
         return ans;
+    }
+    public static int maxi(int arr[]){
+        int m=0;
+        for(int i=0;i<arr.length;i++){
+            m=Math.max(m,arr[i]);
+        }
+        return m;
     }
 }
