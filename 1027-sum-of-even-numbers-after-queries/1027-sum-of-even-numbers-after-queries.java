2c0c294bf2641332e8695b237cc9e4e43abbd71b
V K NITHYANAND <127433644+nithyanandvk@users.noreply.github.com>
2025-08-21 21:21:42 +0530

    Time: 5 ms (59.33%), Space: 49.4 MB (22.49%) - LeetHub

--- 1027-sum-of-even-numbers-after-queries/1027-sum-of-even-numbers-after-queries.java
+++ 1027-sum-of-even-numbers-after-queries/1027-sum-of-even-numbers-after-queries.java
@@ -3,11 +3,32 @@ class Solution {
         int n=nums.length;
         int q=queries.length;
         int ans[]=new int[q];
+        // for(int i=0;i<q;i++){
+        //     nums[queries[i][1]]+=queries[i][0];
+        //     ans[i]=sum(nums);
+        // }
+        int sum=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]%2==0){
+                sum=sum+nums[i];
+            }
+        }
         for(int i=0;i<q;i++){
-            nums[queries[i][1]]+=queries[i][0];
-            ans[i]=sum(nums);
+            int val=queries[i][0];
+            int idx=queries[i][1];
+            if(nums[idx]%2==0){
+                sum=sum-nums[idx];
+            }
+            nums[idx]=nums[idx]+val;
+            if(nums[idx]%2==0){
+                ans[i]=nums[idx]+sum;
+                sum=sum+nums[idx];
+            }else{
+                ans[i]=sum;
+            }
         }
         return ans;
+
     }
     public static int sum(int arr[]){
         int n=arr.length;
