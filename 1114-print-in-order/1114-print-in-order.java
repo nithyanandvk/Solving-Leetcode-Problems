96256a98d441f438209af7a2af303f0d7dd8cb92
V K NITHYANAND <127433644+nithyanandvk@users.noreply.github.com>
2026-03-24 22:56:51 +0530

    Time: 13 ms (11.02%), Space: 44.1 MB (73.16%) - LeetHub

---
+++ 1114-print-in-order/1114-print-in-order.java
@@ -0,0 +1,35 @@
+class Foo {
+    private boolean f=false;
+    private boolean s=false;
+    public Foo() {
+        
+    }
+
+    public synchronized void first(Runnable printFirst) throws InterruptedException {
+        
+        // printFirst.run() outputs "first". Do not change or remove this line.
+        printFirst.run();
+        f=true;
+        notifyAll();
+    }
+
+    public synchronized void second(Runnable printSecond) throws InterruptedException {
+        
+        // printSecond.run() outputs "second". Do not change or remove this line.
+        while(!f){
+            wait();
+        }
+        printSecond.run();
+        s=true;
+        notifyAll();
+    }
+
+    public synchronized void third(Runnable printThird) throws InterruptedException {
+        
+        // printThird.run() outputs "third". Do not change or remove this line.
+        while(!s){
+            wait();
+        }
+        printThird.run();
+    }
+}
