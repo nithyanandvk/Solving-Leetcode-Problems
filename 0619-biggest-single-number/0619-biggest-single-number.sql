2dd1a823cf3d2ccd537b4f17cc5953643677cdf7
V K NITHYANAND <127433644+nithyanandvk@users.noreply.github.com>
2026-04-05 14:05:12 +0530

    Time: 420 ms (43.53%), Space: 0B (100%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 with cte as (
-    select num, count(num) as freq from myNumbers group by num having count(num)=1
+    select num from myNumbers group by num having count(num)=1
 )select max(num) as num from cte;
